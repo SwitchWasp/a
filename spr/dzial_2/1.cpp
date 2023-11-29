8888559728080b4f4d6e97ac38807ef3a7a85422
SwitchWasp <122058629+SwitchWasp@users.noreply.github.com>
2023-11-29 12:53:34 +0100

    Update 1.cpp

--- spr/dzial_2/1.cpp
+++ spr/dzial_2/1.cpp
@@ -5,16 +5,12 @@ using namespace std;
 int main()
 
 {
-
     int liczba;
-
     cout << "Program wypisuje 10 kolejnych wielokrotnosci dodatniej liczby naturalnej" << endl;
 
     cout<<"Podaj liczbe: "<<endl;
-
     cin>>liczba;
-
-
+//petla powtaza sie 10 razy gdzie i pzyjmuje wartosci od 1 do 10 (kolejne wielokrotnosci podanej liczby)
     for(int i = 1; i <= 10; i++) {
         cout << liczba * i << "\n";
     }
