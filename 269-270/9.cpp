f5892abaf1954983da98fa0e51b39188b123fada
SwitchWasp <122058629+SwitchWasp@users.noreply.github.com>
2024-10-27 18:28:59 +0000

    Update 9.cpp

--- 269-270/9.cpp
+++ 269-270/9.cpp
@@ -1,37 +1,38 @@
 #include <iostream>
-
 using namespace std;
 
-const int N=15;
-const int NOMINALY[]={50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
-int ilosc[]={3, 2, 1, 1, 0,0,1,0,0,1,0,0,1,0,0};
+const int N = 15;
+const int NOMINALY[] = {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1};
+int ilosc[] = {3, 2, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0};
 
 void wr(int k, int r[]) {
-    for (int i=0; i<N; i++) {
+    for (int i = 0; i < N; i++) {
+        r[i] = 0;
+    }
+
+    for (int i = 0; i < N; i++) {
+        if (k == 0) { 
+            break;
+        }
         if (ilosc[i] != 0) {
-            if (ilosc[i] == 0) {
-                continue;
-            }
-            if (k/NOMINALY[i] >= ilosc[i]) {
-                k-=NOMINALY[i]*ilosc[i];
-                r[i] = ilosc[i];
-            } else {
-                r[i]=k/NOMINALY[i];
-                k=k%NOMINALY[i];
-            }
+            int c = min(ilosc[i], k / NOMINALY[i]);
+            r[i] = c;
+            k -= c * NOMINALY[i];
         }
     }
 }
 
 int main() {
     int k, r[N];
-    cout << "Podaj kwote w groszah: ";
+    cout << "Podaj kwote w groszach: ";
     cin >> k;
-    wr(k,r);
-    for (int i=0;i<N;i++) {
-        if (r[i]>0) {
-            cout << NOMINALY[i]<<": "<<r[i]<<endl;
+    wr(k, r);
+
+    for (int i = 0; i < N; i++) {
+        if (r[i] > 0) {
+            cout << NOMINALY[i] << ": " << r[i] << endl;
         }
     }
+
     return 0;
 }
